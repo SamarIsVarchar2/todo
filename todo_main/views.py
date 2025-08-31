d9f4b7cfb2393f1bf05e8e90f1bd044528f417eb
SamarIsVarchar2 <balalaasamar@gmail.com>
2025-08-31 10:35:02 +0300

    to-do-app-final

--- todo_main/views.py
+++ todo_main/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')# this mark - to make the last task have been added in the top of the list
-    context = {'tasks': tasks}
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
+   
+    context = {'tasks': tasks, 'completed_tasks':completed_tasks}
     return render(request,'home.html',context)
